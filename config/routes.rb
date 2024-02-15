d08e5644b5c7b89abce798eff3aedf60ee2c73c1
toyo1221 <iyankukku1221@icloud.com>
2024-02-15 18:31:41 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'messages/index'
-  get 'massages/index'
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
+  
+  devise_for :users
+
 end
