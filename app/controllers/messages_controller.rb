f1cdcb8477531a91f046449077091be742116026
toyo1221 <iyankukku1221@icloud.com>
2024-02-17 12:08:20 +0900

    チャットルーム削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
     end
   end
 
-  private
+    private
 
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
